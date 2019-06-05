7f326efb66f70a1f0936c45dfa070aa2b04df8e3
emanovic7 <e_owusu@live.com>
2019-06-05 12:01:26 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  final_array = []
 
+  array.each do |word|
+    if array.length > 1 && word == array.last
+      final_array.push("and " + word)
+    elsif array.length == 2
+      return array.join(" and ")
+    else
+      final_array.push(word)
+    end
+  end
+
+  final_array.join(", ")
 end
